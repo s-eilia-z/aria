193b70c4f35246362ac3dda497dd0c35db10863e
s-eilia-z <seyedeiliazarareh@gmail.com>
2024-07-22 23:45:44 +0330

    bound contracts with repos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,19 @@
 
 namespace App\Providers;
 
+use App\Contracts\AttendanceRepository;
+use App\Contracts\CategoryRepository;
+use App\Contracts\GalleryRepository;
+use App\Contracts\MediaRepository;
+use App\Contracts\PaymentRepository;
+use App\Contracts\ProgramRepository;
 use App\Contracts\UserRepository;
+use App\Repositories\AttendanceEloquentRepository;
+use App\Repositories\CategoryEloquentRepository;
+use App\Repositories\GalleryEloquentRepository;
+use App\Repositories\MediaEloquentRepository;
+use App\Repositories\PaymentEloquentRepository;
+use App\Repositories\ProgramEloquentRepository;
 use App\Repositories\UserEloquentRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(UserRepository::class, UserEloquentRepository::class);
+        $this->app->bind(AttendanceRepository::class, AttendanceEloquentRepository::class);
+        $this->app->bind(CategoryRepository::class, CategoryEloquentRepository::class);
+        $this->app->bind(GalleryRepository::class, GalleryEloquentRepository::class);
+        $this->app->bind(MediaRepository::class, MediaEloquentRepository::class);
+        $this->app->bind(PaymentRepository::class, PaymentEloquentRepository::class);
+        $this->app->bind(ProgramRepository::class, ProgramEloquentRepository::class);
     }
 
     /**
