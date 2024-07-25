a14a5416c377561280fd207e2112174a928f4cbf
s-eilia-z <seyedeiliazarareh@gmail.com>
2024-07-25 15:55:50 +0330

    configured the layout component

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+        <title>{{ $title ?? 'Page Title' }}</title>
+    </head>
+    <body class="h-full" dir="rtl">
+        {{ $slot }}
+    </body>
+</html>
