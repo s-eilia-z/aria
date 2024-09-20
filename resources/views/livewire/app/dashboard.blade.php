00ab11bd3dccbe25dfb1f2863ad396b47a6509d6
seiliaz <e.zarareh@solutions-apps.de>
2024-09-20 17:47:01 +0330

    formatted dashboard.blade.php

--- resources/views/livewire/app/dashboard.blade.php
+++ resources/views/livewire/app/dashboard.blade.php
@@ -4,25 +4,35 @@
             <div class="flex h-16 justify-between">
                 <div class="flex">
                     <div class="flex flex-shrink-0 items-center">
-                        <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-                        <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                        <img class="block h-8 w-auto lg:hidden"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+                        <img class="hidden h-8 w-auto lg:block"
+                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
                     </div>
                     <div class="hidden sm:-my-px sm:mr-6 sm:flex sm:space-x-8 sm:space-x-reverse">
                         <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
-                        <a wire:navigate href="/dashboard" class="mr-5 border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" aria-current="page">داشبورد</a>
+                        <a wire:navigate href="/dashboard"
+                            class="mr-5 border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                            aria-current="page">داشبورد</a>
 
-                        <a wire:navigate href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">برنامه ها</a>
+                        <a wire:navigate href="/programs"
+                            class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">برنامه
+                            ها</a>
 
-                        <a wire:navigate href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">عکس ها</a>
+                        <a wire:navigate href="#"
+                            class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">عکس
+                            ها</a>
                     </div>
                 </div>
                 <div class="hidden sm:mr-6 sm:flex sm:items-center">
                     <!-- Profile dropdown -->
                     <div x-data="{ dropdown: false }" class="relative ml-3">
                         <div>
-                            <button @click="dropdown=!dropdown" type="button" class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                            <button @click="dropdown=!dropdown" type="button"
+                                class="p-2.5 flex justify-center items-center rounded-full bg-slate-100 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="{{ asset('/assets/images/l60Hf.png') }}" alt="">
+                                <i class="fa-regular fa-user text-slate-700"></i>
                             </button>
                         </div>
 
@@ -36,36 +46,48 @@
                             From: "transform opacity-100 scale-100"
                             To: "transform opacity-0 scale-95"
                         -->
-                        <div x-show="dropdown" @click.outside="dropdown = false" class="absolute left-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                        <div x-show="dropdown" @click.outside="dropdown = false" x-transition
+                            class="absolute left-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
+                            tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
-                            @if(auth()->user()->is_admin)
-                                <a wire:navigate href="#" class="text-center block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">پنل ادمین</a>
+                            @if (auth()->user()->is_admin)
+                                <a wire:navigate href="#"
+                                    class="text-center block px-4 py-2 text-sm text-gray-700" role="menuitem"
+                                    tabindex="-1" id="user-menu-item-0">پنل ادمین</a>
                             @endif
 
-                            <a wire:navigate href="#" class="text-center block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">تنظیمات</a>
+                            <a wire:navigate href="#" class="text-center block px-4 py-2 text-sm text-gray-700"
+                                role="menuitem" tabindex="-1" id="user-menu-item-1">تنظیمات</a>
 
-                            <a wire:navigate href="/logout" class="text-center block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">خروج</a>
+                            <a wire:navigate href="/logout" class="text-center block px-4 py-2 text-sm text-gray-700"
+                                role="menuitem" tabindex="-1" id="user-menu-item-2">خروج</a>
                         </div>
                     </div>
                 </div>
                 <div class="-mr-2 flex items-center sm:hidden">
                     <!-- Mobile menu button -->
-                    <button @click="menuOpen = !menuOpen" type="button" class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" aria-controls="mobile-menu" aria-expanded="false">
+                    <button @click="menuOpen = !menuOpen" type="button"
+                        class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        aria-controls="mobile-menu" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <!--
                           Heroicon name: outline/bars-3
 
                           Menu open: "hidden", Menu closed: "block"
                         -->
-                        <svg x-show="!menuOpen" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        <svg x-show="!menuOpen" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
                         <!--
                           Heroicon name: outline/x-mark
 
                           Menu open: "block", Menu closed: "hidden"
                         -->
-                        <svg x-show="menuOpen" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                        <svg x-show="menuOpen" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
@@ -77,13 +99,19 @@
         <div x-show="menuOpen" @click.outside="menuOpen=false" x-transition class="sm:hidden" id="mobile-menu">
             <div class="space-y-1 pt-2 pb-3">
                 <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" -->
-                <a wire:navigate href="/dashboard" class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium" aria-current="page">داشبورد</a>
+                <a wire:navigate href="/dashboard"
+                    class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                    aria-current="page">داشبورد</a>
 
-                <a href="#" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">برنامه ها</a>
+                <a href="#"
+                    class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">برنامه
+                    ها</a>
 
-                <a href="#" class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">عکس ها</a>
+                <a href="#"
+                    class="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">عکس
+                    ها</a>
             </div>
-            <div x-data="{userData: false}" class="border-t border-gray-200 pt-4 pb-3">
+            <div x-data="{ userData: false }" class="border-t border-gray-200 pt-4 pb-3">
                 <div @click="userData=!userData" @click.outside="userData=false" class="flex items-center px-4">
                     <div class="ml-3">
                         <div class="text-base font-medium text-gray-800">Tom Cook</div>
@@ -91,13 +119,17 @@
                     </div>
                 </div>
                 <div x-show="userData" class="mt-3 space-y-1">
-                    @if(auth()->user()->is_admin)
-                        <a wire:navigate href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">پنل ادمین</a>
+                    @if (auth()->user()->is_admin)
+                        <a wire:navigate href="#"
+                            class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">پنل
+                            ادمین</a>
                     @endif
 
-                    <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">تنظیمات</a>
+                    <a href="#"
+                        class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">تنظیمات</a>
 
-                    <a wire:navigate href="/logout" class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">خروج</a>
+                    <a wire:navigate href="/logout"
+                        class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800">خروج</a>
                 </div>
             </div>
         </div>
